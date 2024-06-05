2bd17561b719140d8562708032ac45fb6878ae57
Georgemugo <georgepmash@gmail.com>
2024-06-05 12:03:06 +0300

    Adds validation

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,15 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+
+
+
+#inheritance
+'''
+In Python, classes can inherit from one another. 
+This means that they acquire all of the attributes and behaviors (i.e. all of the methods)
+ of the parent, also called the superclass. 
+'''
